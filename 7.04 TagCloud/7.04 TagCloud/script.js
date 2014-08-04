a93caae8fcb27d94f68ec7f6e553e47db97f2afb
Marian Graurov <marian_graurov@abv.bg>
2014-08-04 11:41:44 +0300

    7.04 exercise

---
+++ 7.04 TagCloud/7.04 TagCloud/script.js
@@ -0,0 +1,54 @@
+﻿function MakeTagCloud(tags, minFontSize, maxFontSize) {
+    var notRepeatingTags = [];
+    var numberOfRepetition = [];
+    var currentPosition = 0;
+    var isRepeated = false;
+    for (var i = 0; i < tags.length; i++) {
+        for (var j = 0; j < currentPosition; j++) {
+            if (tags[j].toLowerCase() === tags[i].toLowerCase()) {
+                isRepeated = true;
+                break;
+            }
+        }
+        if (!isRepeated) {
+
+            element = tags[i].toLowerCase()
+            notRepeatingTags.push(element);
+            numberOfRepetition.push(1);
+        }
+        else {
+            for (var k = 0; k < notRepeatingTags.length; k++) {
+                if (notRepeatingTags[k] === tags[i]) {
+                    numberOfRepetition[k] += 1;
+                    break;
+                }
+            }
+            isRepeated = false;
+        }
+        currentPosition++;
+    }
+    
+    var maxRepetition = 0;
+
+    for (var i = 0; i < numberOfRepetition.length; i++) {
+        if (maxRepetition < numberOfRepetition[i]) {
+            maxRepetition = numberOfRepetition[i];
+        }
+    }
+
+    var div = document.createElement('div');
+
+
+    for (var i = 0; i < notRepeatingTags.length; i++) {
+        var result = document.createElement('span');
+        result.style.fontSize = CurrentFontSizeCalculator(numberOfRepetition[i], maxRepetition, minFontSize, maxFontSize) + 'px';
+        result.appendChild(document.createTextNode(tags[i] + ' '));
+        div.appendChild(result);
+    }
+
+    document.body.appendChild(div);
+}
+
+function CurrentFontSizeCalculator(currentRepetition, maxRepetition, minFontSize, maxFontSize) {
+    return minFontSize + (maxFontSize - minFontSize) * currentRepetition / maxRepetition;
+}
