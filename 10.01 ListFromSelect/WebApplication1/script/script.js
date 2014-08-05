ec3443edf3a17e0b9f2d15437a524414ab67932f
Marian Graurov <marian_graurov@abv.bg>
2014-08-05 17:48:37 +0300

    9.03,9.04,10.01

---
+++ 10.01 ListFromSelect/WebApplication1/script/script.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function(){
+    $("#dropdown").dropdown();
+})
+
+$.fn.dropdown = function (ev) {
+    $this = $(this);
+    var elements = [];
+    var $target = $("option");
+    for (var i = 0, len = $target.length; i < len; i++) {
+        elements.push($($target[i]).html());
+    }
+    $(this).css({"display":"none"});
+    var $div = $('<div class="dropdown-list-container"><div>').appendTo('body');
+    var $ul = $('<ul class="dropdown-list-options"></ul>').appendTo($div);
+    for (var i = 0, len = elements.length; i < len; i++) {
+        $('<li class="dropdown-list-option" data-value="' + i + '">' + elements[i] + '</li>').appendTo($ul);
+    }
+
+
+}
---
+++ 9.04 BackgroundColor/9.04 BackgroundColor/Script/script.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+    $('#color').on('change', function () {
+        ChangeColor();
+    }
+    )
+});
+
+function ChangeColor() {
+    var color = $('#color').val();
+    $('body').css('background', color);
+}
