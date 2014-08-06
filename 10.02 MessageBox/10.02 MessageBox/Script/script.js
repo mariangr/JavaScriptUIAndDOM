f2ef5c899b12a294ba34dde40464fb09e9641d36
Marian Graurov <marian_graurov@abv.bg>
2014-08-06 13:05:58 +0300

    10.02 MessageBox

---
+++ 10.02 MessageBox/10.02 MessageBox/Script/script.js
@@ -0,0 +1,42 @@
+﻿$(document).ready(function () {
+    var msgBox = $('#message-box').messageBox();
+
+    $('#error-btn').on('click', function () {
+        msgBox.error('ERROR', 'red')
+    });
+
+    $('#success-btn').on('click', function () {
+        msgBox.success('SUCCESS', 'green')
+    });
+});
+
+$.fn.messageBox = function () {
+    var $this = $(this);
+
+    function showMessage(message, backgroundColor) {
+        var $result = $('#message-box-result');
+
+        $result.html(message);
+        $result.css('background-color', backgroundColor);
+
+        $result.stop().fadeIn(3000, function () {
+            $result.fadeOut(3000, function () {
+                $result.html('');
+            });
+        });
+    }
+
+    return {
+        success: function () {
+            showMessage('success', 'lightgreen');
+
+            return $this;
+        },
+        error: function () {
+            showMessage('error', 'red');
+
+            return $this;
+        }
+    };
+}
+
